d55a3ef5f934cc0d0a6b1370974f8c8bd0180034
Vladimir Chernov <dgudo73@mail.ru>
2019-12-05 17:26:26 +0300

    Адаптировал footre

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function(){
 
-$('.header__slider').slick ({
-	dots: true,
-	infinite: true,
-	fade: true,
-	// prevArrow: false,
-	// nextArrow: false,
-	arrows: false,
-});
+	$('.header__slider').slick ({
+		dots: true,
+		infinite: true,
+		fade: true,
+		// prevArrow: false,
+		// nextArrow: false,
+		arrows: false,
+	});
 	
 	let titleElem = document.querySelectorAll('.footer__title');
 
@@ -21,63 +21,6 @@ $('.header__slider').slick ({
 
 
 });
-
-// document.getElementById('nav').onmouseover = function (event) {
-// 	var target = event.target;
-// 	if (target.className == 'header__sup-item') {
-// 		var s = target.getElementsByClassName('submenu');
-// 		closeMenu();
-// 		s[0].style.display = 'block';
-// 	}
-// }
-
-// document.onmouseover = function (event) {
-// 	var target = event.target;
-// 	console.log(event.target);
-// 	if (target.className != 'menu-item' && target.className != 'submenu') {
-// 		closeMenu();
-// 	}
-// }
-
-// function closeMenu() {
-// 	var menu = document.getElementById('nav');
-// 	var subm = document.getElementsByClassName('submenu');
-// 	for (var i = 0; i < subm.length; i++) {
-// 		subm[i].style.display = "none";
-// 	}
-// }
-
-// var el = document.getElementsByClassName('submenu__list-item');
-// for (var i = 0; i < el.length; i++) {
-// 	el[i].addEventListener('mouseenter', showSub, false);
-// 	el[i].addEventListener('mouseleave', hideSub, false);
-// }
-
-// function showSub() {
-// 	if (this.children.length > 1) {
-// 		this.children[1].style.height = "auto";
-// 		this.children[1].style.opacity = "1";
-// 		this.children[1].style.overflow = "visible";
-// 	} else {
-// 		return false;
-// 	}
-// }
-
-// function hideSub() {
-// 	if (this.children.length > 1) {
-// 		this.children[1].style.height = "0";
-// 		this.children[1].style.opacity = "0";
-// 		this.children[1].style.overflow = "hidden";
-// 	} else {
-// 		return false;
-// 	}
-// }
-
-// $('.header__bank.with-sub').on('click', function (e) {
-// 	e.preventDefault();
-// 	$(this).addClass('.opened')
-// });
-
 
 // О Банке
 let opend = document.querySelector('.opend');
@@ -161,47 +104,4 @@ body.addEventListener('click', function (e) {
 		submenu4.style.display = "none";
 	}
 });
-
-//Аккордеон
-
-// function accordion(selector) {
-// 	let accordion = $(selector);
-// 	let spans = accordion.children('span');
-// 	let divs = accordion.children('div');
-
-// 	accordion
-// 	.addClass('accordion')
-// 	.on('click', '.accordion__control', function (e) {
-// 		e.preventDefault();
-
-// 		let a = $(this);
-// 		let active = a.hasClass('accordion__control_active');
-
-// 		accordion
-// 			.find('.accordion__control_active')
-// 			.removeClass('accordion__control_active');
-// 		accordion
-// 			.find('.accordion__panel_active')
-// 			.removeClass('accordion__panel_active');
-
-// 		if (!active) {
-// 			a
-// 				.addClass('accordion__control_active')
-// 				.next()
-// 				.addClass('accordion__panel_active');
-// 		}
-// 	});
-
-// 	spans.each(function (){
-// 		let span = $(this);
-// 		span.replaceWith(
-// 			'<a href="#" class="accordion__control">'
-// 			+ span.text()
-// 			+ '</a>'
-// 		);
-// 	});
-// 	divs.addClass('accordion__panel')
-// }
-
-// accordion('#accordion');
 
