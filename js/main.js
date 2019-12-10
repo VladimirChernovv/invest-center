dded0fa5820becbff72ae069581dccee845765c9
Vladimir Chernov <dgudo73@mail.ru>
2019-12-10 13:08:18 +0300

    Начал делать слайдер для карточек в market

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+//import { exists } from "fs";
+
 $(function(){
 
 	$('.header__slider').slick ({
+		dots: true,
+		infinite: true,
+		fade: true,
+		arrows: false,	
+	});
+
+	$('.market__slider').slick({
 		dots: true,
 		infinite: true,
 		fade: true,
@@ -8,15 +17,16 @@ $(function(){
 	});
 
 	//Открывающееся верхнее меню
-	// let titleWrap = document.querySelectorAll('.header__title-wrap');
+	let titleWrap = document.querySelectorAll('.header__title-wrap');
 
-	// for (let i = 0; i < titleWrap.length; i++) {
-	// 	titleWrap[i].addEventListener('click', show_list)
-	// }
+	for (let i = 0; i < titleWrap.length; i++) {
+		titleWrap[i].addEventListener('click', show_list)
+	}
 
-	// function show_list(e) {
-	// 	e.target.nextElementSibling.classList.toggle('open');
-	// }
+	function show_list(e) {
+		e.target.nextElementSibling.classList.toggle('open');
+
+	}
 
 	
 	//Открывающееся нижнее меню в footer
@@ -27,10 +37,14 @@ $(function(){
 	}
 
 	function show_list(e) {
-		e.target.nextElementSibling.classList.toggle('open');
+		
+		if (e.target.nextElementSibling.children[0].classList[0] === "header__menu-lower"){
+			e.target.nextElementSibling.children[0].classList.toggle('open');
+			e.target.nextElementSibling.children[1].classList.toggle('open');
+		} else {
+			e.target.nextElementSibling.classList.toggle('open');
+		}
 	}
-
-
 });
 
 // О Банке
@@ -38,6 +52,7 @@ let opend = document.querySelector('.opend');
 
 opend.addEventListener('click', function (e) {
 	e.stopPropagation()
+	
 
 	let submenu1 = document.querySelector('.submenu1')
 
@@ -94,25 +109,28 @@ bonds.addEventListener('click', function (e) {
 let body = document.querySelector('body');
 
 body.addEventListener('click', function (e) {
-	
-	let submenu1 = document.querySelector('.submenu1');
-	if (submenu1.style.display === "block") {
-		submenu1.style.display = "none";
-	}
 
-	let submenu2 = document.querySelector('.submenu2');
-	if (submenu2.style.display === "block") {
-		submenu2.style.display = "none";
-	}
+	if (e.target.classList[0] !== "header__title-wrap") {
 
-	let submenu3 = document.querySelector('.submenu3');
-	if (submenu3.style.display === "block") {
-		submenu3.style.display = "none";
-	}
+		let submenu1 = document.querySelector('.submenu1');
+		if (submenu1.style.display === "block") {
+			submenu1.style.display = "none";
+		}
 
-	let submenu4 = document.querySelector('.submenu4');
-	if (submenu4.style.display === "block") {
-		submenu4.style.display = "none";
+		let submenu2 = document.querySelector('.submenu2');
+		if (submenu2.style.display === "block") {
+			submenu2.style.display = "none";
+		}
+
+		let submenu3 = document.querySelector('.submenu3');
+		if (submenu3.style.display === "block") {
+			submenu3.style.display = "none";
+		}
+
+		let submenu4 = document.querySelector('.submenu4');
+		if (submenu4.style.display === "block") {
+			submenu4.style.display = "none";
+		}
 	}
 });
 
